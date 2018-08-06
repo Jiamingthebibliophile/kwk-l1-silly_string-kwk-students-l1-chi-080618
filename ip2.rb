20be975c1c9d1ae6ec62686d856122642375eb00
Jiaming Lou <stylerlight101@gmail.com>
2018-08-06 20:25:24 +0000

    automatically backed up by learn

--- ip2.rb
+++ ip2.rb
@@ -2,7 +2,7 @@ def my_fav_things
   
 fav_animal = "monkeys"
 fav_place = "Chicago"
-fav_arist = "Taylor Swift"
+fav_artist = "Taylor Swift"
 
   puts "I love #{fav_animal}"
   puts "I love #{fav_place}"
